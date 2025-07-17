8f02cd10d00745f2ab0afad8aa61bc355c1e6726
vinodh1988 <k.c.vinodh@hotmail.com>
2025-07-17 10:12:15 +0530

    latest

--- resource.tf
+++ resource.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "terraazure" {
   name     = "terraazure"
   location = "East US"
 }
+
+resource "azurerm_resource_group" "terranow" {
+  name     = "terranow"
+  location = "East US"
+}
